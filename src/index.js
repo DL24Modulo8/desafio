aebf596e76041bf7cabdb8ed3df678b625e0bb19
Eduardo Chasco <echasco@outlook.com>
2025-03-24 19:40:05 -0300

    tasks routes

--- src/index.js
+++ src/index.js
@@ -14,6 +14,19 @@ app.get("/todos", (req, res) => {
         { id: 2, tarea: "Configurar Express" },
     ]);
 });
+
+// Ruta /tasks: responde un mensaje genérico
+app.get("/tasks", (req, res) => {
+    res.send("Bienvenido a la ruta Tasks");
+});
+
+// Ruta /tasks/:id: responde un mensaje genérico
+app.get("/tasks/:id", (req, res) => {
+    const { id } = req.params;
+    res.send(`Estás consultando la tarea con ID: ${id}`);
+});
+
+
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
